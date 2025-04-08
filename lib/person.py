67cfa83de91556332445dd2724baceceb27a2b91
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-04-08 20:54:54 +0300

    Task complete

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+"""APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -13,7 +13,33 @@ APPROVED_JOBS = [
     "Research & Development",
     "Marketing",
     "Purchasing"
-]
+] """
 
 class Person:
-    pass
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+
+    def __init__(self, name="Person", job="Admin"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in Person.approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
